096ae07a59320befdee175b6faa7ed0e3bdb7663
Sandfox <134222969+losersandfox@users.noreply.github.com>
2024-06-13 00:17:47 +0800

    Init

---
+++ Server/Cypher-server-side/src/main/java/com/example/cypherserverside/CypherServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.example.cypherserverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CypherServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CypherServerSideApplication.class, args);
+	}
+
+}
---
+++ Server/Cypher-server-side/src/test/java/com/example/cypherserverside/CypherServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cypherserverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CypherServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
